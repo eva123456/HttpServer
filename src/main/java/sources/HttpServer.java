9fb1188c42f8f55b6d6e910f78c991af447e5150
Eva <eva24372@gmail.com>
2017-04-04 20:01:00 +0300

    cmd params added

--- src/main/java/sources/HttpServer.java
+++ src/main/java/sources/HttpServer.java
@@ -5,26 +5,58 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 
+import static javafx.application.Platform.exit;
+
 /**
  * Created by eva on 02.04.17.
  */
 public class HttpServer {
-    private static int PORT = 8080;
-    private static HashMap<String, String> params;
+    private static int PORT;
+    private static int NCPU;
+    private static String ROOTDIR;
+    private static HashMap<String, Object> params = new HashMap<>(3);
 
     public static void main(String[] args) throws IOException {
-        //command-parser needed
-        for(int i = 0; i < args.length; ++i){
-            System.out.println(args[i]);
-        }
+
+        fillParams(args);
+
+        PORT = (Integer) params.get("-p");
+        NCPU = (Integer) params.get("-c");
+        ROOTDIR = (String) params.get("-r");
 
         ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Start at port 8080!");
+        System.out.println("Server started on port " + PORT);
         while (true){
             Socket socket = serverSocket.accept();
             new Thread(new SocketTask(socket)).start();
         }
 
+    }
+
+    private static void fillParams(String[] args){
+        params.put("-r", FileStorage.DOCUMENT_ROOT);
+        params.put("-c", null);
+        params.put("-p", 8080);
+
+        for(int i = 0; i < args.length; i+=2){
+            if (params.containsKey(args[i])){
+                if(args[i].equals("-r")){
+                    params.put(args[i], args[i+1]);//regexp?
+                } else {
+                    try {
+                        params.put(args[i], Integer.parseInt(args[i+1]));
+                    } catch (Exception exception){
+                        System.out.println("Unknown parameter value " + args[i+1] + "for" + args[i]);
+                        System.exit(0);
+                    }
+                }
+            }
+            else{
+                System.out.println("Unknown option " + args[i]);
+                System.out.println("Usage -r ROOTDIR, -c NCPU, [-p PORT]");
+                System.exit(0);
+            }
+        }
     }
 
     private static class SocketTask implements Runnable{
